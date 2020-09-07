59594ce933962770cee2545a5390664ebf535116
manvendupathak-dot <manvendupathak2000@gmail.com>
2020-09-07 12:53:12 +0530

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Manvendu Pathak on 07/09/20.
+//  Copyright © 2020 Manvendu Pathak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
